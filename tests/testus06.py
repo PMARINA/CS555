b3f8534bf74eba3c6c506e62624524c33dc9fef4
grinchpal <47698676+grinchpal@users.noreply.github.com>
2021-10-04 22:42:38 -0400

    [MERGE] US05 complete fixed

--- tests/testus06.py
+++ tests/testus06.py
@@ -1,25 +1,25 @@
 import unittest
 
-from gedutil import US05, GED_Line, GED_Tag, Parser
+from gedutil import US06, GED_Line, GED_Tag, Parser
 
 
-class TestUS05(unittest.TestCase):
+class TestUS06(unittest.TestCase):
     """
     This test checks to see that death only occurred after marriage.
 
     """
 
     def test_deat(self):
-        u = US05()
-        p = Parser("input_files/US05/deat_before_marr.ged")
+        u = US06()
+        p = Parser("input_files/US06/deat_before_div.ged")
         p.read()
         p.parse()
         with self.assertRaises(ValueError):
             u.run()
 
     def test_valid(self):
-        u = US05()
-        p = Parser("input_files/US05/control.ged")
+        u = US06()
+        p = Parser("input_files/US06/control.ged")
         p.read()
         p.parse()
         u.run()
