bf3bcc945de9054371a419c2521e68e0d7bb7c0e
PMARINA <bluerat33@gmail.com>
2021-10-10 23:00:10 -0400

    [REFACTOR] Add breaking change to bring up to speed
    - This commit breaks the branch, BUT
    - Upon merging into main with PM_hw06, will work fine

--- gedutil/gedutil/checks/us05.py
+++ gedutil/gedutil/checks/us05.py
@@ -3,9 +3,11 @@ from datetime import timedelta
 
 from dateutil.parser import parse as parseDate
 
+from gedutil.base import ID, GED_Tag
 from gedutil.mongo_client import families, individuals
 
 from .check import Check
+from .utils.get_fam_info import get_parents_from_doc
 
 
 class US05(Check):
@@ -19,22 +21,17 @@ class US05(Check):
 
         # Go through the families
         for doc in families.find():
-            if "wife" not in doc:
-                ids_of_people = []
-            else:
-                ids_of_people = doc["wife"]
-            if "husb" in doc:
-                ids_of_people.extend(doc["husb"])
-            if "marr" not in doc:
+            ids_of_people = get_parents_from_doc(doc)
+            if GED_Tag.MARR.name not in doc:
                 continue
-            marriage_date = parseDate(doc["marr"])
+            marriage_date = parseDate(doc[GED_Tag.MARR.name])
             # latest_birth_date = marriage_date - timedelta(days=365 * 14)
             for id in ids_of_people:
-                person = individuals.find_one({"ged_id": id})
-                if "deat" not in person:
+                person = individuals.find_one({ID.IND_ID.name: id})
+                if GED_Tag.DEAT.name not in person:
                     f"This person does not have a recorded death date."
                     continue
-                deat_date = parseDate(person["deat"])
+                deat_date = parseDate(person[GED_Tag.DEAT.name])
                 if deat_date < marriage_date:
                     raise ValueError(
                         f"US05 - {deat_date} occured before marriage on {marriage_date}."
