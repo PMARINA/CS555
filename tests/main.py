0621b76fe6472787c914f57e1bff575f9b70b0f0
PMARINA <bluerat33@gmail.com>
2021-10-13 14:22:18 -0400

    [REFACTOR] Complete pytest config
    - Options now setup

--- tests/main.py
+++ tests/main.py
@@ -2,10 +2,12 @@ from loguru import logger
 
 from gedutil import Parser, Tabular_Output
 
+from .path_util import stabilize
+
 
 @logger.catch
 def main():
-    parser = Parser("input_files/main/control.ged")
+    parser = Parser(stabilize("", "main/control.ged"))
     parser.read()
     parser.parse()
     # parser.print_input_output_project_2_assignment()
