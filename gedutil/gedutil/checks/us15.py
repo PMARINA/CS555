d686e9a204c29bfe8684d5b6a58b00e358a8329f
mnthan <72927773+mnthan@users.noreply.github.com>
2021-10-17 02:36:54 -0400

    user stories updated

--- gedutil/gedutil/checks/us15.py
+++ gedutil/gedutil/checks/us15.py
@@ -12,26 +12,13 @@ import ErrorLogger
 # User Story #15: A family must have less than 15 siblings.
 # -----------------------------------------------------------------------------
 class US15:
-def less_than_15_siblings(the_family):
-    """
-    ---------------------------------------------------------------------------
-    This function checks whether the family contains less than 15 siblings.
-    User Story #15.
-    Parameters
-    ----------
-    the_family:      The family object to test whether there are less than 15
-                     siblings.
-    Returns
-    -------
-    True:  When there are less than 15 siblings in the family.
-    False: When there are 15 or more siblings in the family.
-    ---------------------------------------------------------------------------
-    """
-    result = True
+    def less_than_15_siblings(the_family):
+       
+        result = True
 
-    if len(the_family.children) >= 15:
-        result = False
-        ErrorLogger.__logError__(ErrorLogger._FAMILY, "US15", the_family.id, \
-            "There are too many (" + str(len(the_family.children)) + ") siblings.")
+        if len(the_family.children) >= 15:
+            result = False
+            ErrorLogger.__logError__(ErrorLogger._FAMILY, "US15", the_family.id, \
+                                     "There are too many (" + str(len(the_family.children)) + ") siblings.")
 
-    return result
+        return result
