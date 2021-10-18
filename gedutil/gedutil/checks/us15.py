c03e23617b7cbc7e05745ad966f2956b33b481f2
mnthan <72927773+mnthan@users.noreply.github.com>
2021-10-17 20:56:36 -0400

    updated files

--- gedutil/gedutil/checks/us15.py
+++ gedutil/gedutil/checks/us15.py
@@ -6,13 +6,65 @@ Created on Fri Oct 15 17:35:32 2021
 @author: manthan
 """
 
-import ErrorLogger
+import logging
+import logging.config
+from logging import FileHandler
 
 
 # -----------------------------------------------------------------------------
 # User Story #15: A family must have less than 15 siblings.
 # -----------------------------------------------------------------------------
 class US15:
+
+    logger = logging.getLogger("gedcomLogger")
+    fileHandler = logging.FileHandler("gedcomError.log", "w")
+
+    _logMessages = [""]
+    _ANOMALY = "ANOMALY"
+    _ERROR = "ERROR"
+    _INDIVIDUAL = "INDIVIDUAL"
+    _FAMILY = "FAMILY"
+    _GENERAL = "GENERAL"
+
+    def __initLogger__(default=""):
+        #        logger.config.dictConfig(GEDCOM_LOGGER)
+        US15.logger.addHandler(US15.fileHandler)
+        US15.logger.setLevel(logging.DEBUG)
+
+    def __logAnomaly__(recordType, userStory, id, messsage):
+        msg = (
+            US15._ANOMALY
+            + ": "
+            + recordType
+            + ": "
+            + userStory
+            + ": "
+            + str(id)
+            + ": "
+            + messsage
+        )
+        US15.logger.info(msg)
+        US15._logMessages.append(msg)
+
+    def __logError__(recordType, userStory, id, messsage):
+        msg = (
+            US15._ERROR
+            + ": "
+            + recordType
+            + ": "
+            + userStory
+            + ": "
+            + str(id)
+            + ": "
+            + messsage
+        )
+        US15.logger.error(msg)
+        US15._logMessages.append(msg)
+
+    def __printLogMessages__(default=""):
+        for message in US15._logMessages:
+            print(message)
+
     def __init__(self):
         pass
 
@@ -25,8 +77,8 @@ class US15:
 
         if len(the_family.children) >= 15:
             result = False
-            ErrorLogger.__logError__(
-                ErrorLogger._FAMILY,
+            US15.__logError__(
+                US15._FAMILY,
                 "US15",
                 the_family.id,
                 "There are too many (" + str(len(the_family.children)) + ") siblings.",
