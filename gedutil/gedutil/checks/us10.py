85f1e6f4fe8abb2d99dddddd9968df8d84f10916
PMARINA <bluerat33@gmail.com>
2021-10-18 21:05:43 -0400

    [BUILD] Convert US10 to output messages

--- gedutil/gedutil/checks/us10.py
+++ gedutil/gedutil/checks/us10.py
@@ -3,14 +3,16 @@ from datetime import timedelta
 
 from dateutil.parser import parse as parseDate
 
-from gedutil.base import ID, GED_Tag
-from gedutil.mongo_client import families, individuals
+from gedutil.base import ID, Error_Type, GED_Tag, User_Story
+from gedutil.mongo_client import errors, families, individuals
 
 from .check import Check
 from .utils.get_fam_info import get_parents_from_doc
 
 
 class US10(Check):
+    MINIMUM_MARRIAGE_AGE_YEARS = 14
+
     def __init__(self):
         pass
 
@@ -24,7 +26,9 @@ class US10(Check):
             if GED_Tag.MARR.name not in doc:
                 continue
             marriage_date = parseDate(doc[GED_Tag.MARR.name])
-            latest_birth_date = marriage_date - timedelta(days=365 * 14)
+            latest_birth_date = marriage_date - timedelta(
+                days=365 * US10.MINIMUM_MARRIAGE_AGE_YEARS
+            )
 
             ids_of_people = get_parents_from_doc(doc)
             for id in ids_of_people:
@@ -34,6 +38,10 @@ class US10(Check):
                     continue
                 birth_date = parseDate(person[GED_Tag.BIRT.name])
                 if birth_date >= latest_birth_date:
-                    raise ValueError(
-                        f"US10 - {birth_date} too close to marriage date: {marriage_date}"
+                    errors.insert_one(
+                        {
+                            "user story": User_Story.US10.name,
+                            "error type": Error_Type.ANOMALY.name,
+                            "message": f"Birth date ({birth_date}) within {US10.MINIMUM_MARRIAGE_AGE_YEARS} years of marriage ({marriage_date})",
+                        }
                     )
