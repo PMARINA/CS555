88b3d75985ddd9f336e67b9366d4365184b9b851
mnthan <72927773+mnthan@users.noreply.github.com>
2021-11-11 15:55:57 -0500

    US 17 updated

---
+++ tests/individual.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 19:15:38 2021
+
+@author: manthan
+"""
+
+from datetime import date, datetime
+
+import date_diff_calculator
+import ErrorLogger
+
+
+class Individual:
+    def __init__(self):
+        self.type = "I"
+        self.id = ""
+        self.firstAndMiddleName = ""
+        self.lastname = ""
+        self.name = ""
+        self.gender = ""
+        self.birthDate = None
+        self.deathDate = None
+        self.children = []
+        self.spouse = []
+        self.familyIdChild = None
+        self.familyIdSpouse = None
+        self.birthDateStr = "NA"
+        self.deathDateStr = "NA"
+        self.childrenStr = "NA"
+        self.spouseStr = "NA"
+        self.age = -1
+        self.alive = False
+
+    def toString(self):
+        self.alive = self.deathDate is None
+        if self.birthDate is not None:
+            try:
+                self.birthDateStr = self.birthDate.strftime("%d %b %Y")
+            except:
+                ErrorLogger.__logError__(
+                    ErrorLogger._INDIVIDUAL,
+                    "N/A",
+                    self.id,
+                    "Unrecognizable birth date.",
+                )
+        self.calculateAge()
+        if self.deathDate is not None:
+            self.deathDateStr = self.deathDate.strftime("%d %b %Y")
+        if len(self.children) > 0:
+            self.childrenStr = str(self.children)
+
+        ##2/15/18 testing found error if statement below should be checking for spouse, not children
+        if len(self.spouse) > 0:
+            self.spouseStr = str(self.spouse)
+
+    def calculateAge(self):
+        if self.birthDate is not None:
+            calculatedAge = date_diff_calculator.calculateDateDifference(
+                self.birthDate, self.deathDate, "years"
+            )
+            if calculatedAge is not None:
+                self.age = str(calculatedAge)
+        else:
+            ErrorLogger.__logError__(
+                ErrorLogger._INDIVIDUAL,
+                "US27",
+                self.id,
+                "Can not determine age, individual has no Birth Date",
+            )
+        if not self.alive and self.deathDate is None:
+            ErrorLogger.__logError__(
+                ErrorLogger._INDIVIDUAL,
+                "US27",
+                self.id,
+                "Age Calculation may be off, individual is not alive, but no Death Date was available",
+            )
+        if date_diff_calculator.isSecondNumberBigger(int(self.age), 0):
+            ErrorLogger.__logError__(
+                ErrorLogger._INDIVIDUAL,
+                "US27",
+                self.id,
+                "Age Calculation may be off, individual age is less than 0",
+            )
+        ##US7 call isAgeLessThan150
+        if not self.isAgeLessThan150():
+            ErrorLogger.__logAnomaly__(
+                ErrorLogger._INDIVIDUAL, "US07", self.id, "Older than 150"
+            )
+
+    ##US7 determine if age is less than 150
+    def isAgeLessThan150(self):
+        if not date_diff_calculator.isSecondNumberBigger(int(self.age), 150):
+            return False
+        else:
+            return True
+
+        # US31 get individual and return its details in GED format
+
+    def isSingleAliveOver30(self):
+        retValue = False
+        try:
+            retValue = int(self.age) > 30 and self.alive and len(self.spouse) == 0
+        except:
+            retValue = False
+
+        return retValue
+
+
+# For story 01 - generic function to compare input date with current date, return true if input date is bigger than current date
+def compareDates(tmpDate):
+    return tmpDate > datetime.now()
---
+++ tests/test_us17.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 19:03:12 2021
+
+@author: manthan
+
+"""
+
+import sys
+import unittest
+
+import family
+import family_relationships
+import individual
+
+
+class Test_marriageToDecendants(unittest.TestCase):
+    def test_MarriageToChild(self):
+        mom = individual.Individual()
+        mom.id = "1"
+        dad = individual.Individual()
+        dad.id = "2"
+        dad.spouse = ["1"]
+        mom.spouse = ["2", "3"]
+        child = individual.Individual()
+        child.id = "3"
+        child.spouse = ["1"]
+        newFamily = family.Family()
+        newFamily.id = "F1"
+        newFamily.children = ["3"]
+        newFamily.husbandId = "1"
+        newFamily.wifeId = "2"
+
+        familyDic = {}
+        familyDic["F1"] = newFamily
+        individualDic = {}
+        individualDic[mom.id] = mom
+        individualDic[dad.id] = dad
+        individualDic[child.id] = child
+
+        self.assertFalse(
+            family_relationships.validParentDecendantMarriages(familyDic, individualDic)
+        )
+
+    def test_NoMarriageToChild(self):
+        mom = individual.Individual()
+        mom.id = "1"
+        dad = individual.Individual()
+        dad.id = "2"
+        dad.spouse = ["1"]
+        mom.spouse = ["2"]
+        child = individual.Individual()
+        child.id = "3"
+        newFamily = family.Family()
+        newFamily.id = "F1"
+        newFamily.children = ["3"]
+        newFamily.husbandId = "1"
+        newFamily.wifeId = "2"
+
+        familyDic = {}
+        familyDic["F1"] = newFamily
+        individualDic = {}
+        individualDic[mom.id] = mom
+        individualDic[dad.id] = dad
+        individualDic[child.id] = child
+
+        self.assertTrue(
+            family_relationships.validParentDecendantMarriages(familyDic, individualDic)
+        )
+
+
+if __name__ == "__main__":
+    """
+    if len(sys.argv) != 2:
+            sys.exit("ERROR need GEDCOM file on command line")
+    command_line = sys.argv[1]
+    del sys.argv[1:]
+    """
+    unittest.main()
